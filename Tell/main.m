554ba223f2793f7215c0c081dc99838946f1c50a
Ben Liong <benliong@gmail.com>
2014-06-08 19:45:03 +0800

    Initial Commit

---
+++ Tell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tell
+//
+//  Created by Ben Liong on 8/6/14.
+//  Copyright (c) 2014 Pixelicious Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ACAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ACAppDelegate class]));
+    }
+}
---
+++ TellTests/TellTests.m
@@ -0,0 +1,34 @@
+//
+//  TellTests.m
+//  TellTests
+//
+//  Created by Ben Liong on 8/6/14.
+//  Copyright (c) 2014 Pixelicious Software. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TellTests : XCTestCase
+
+@end
+
+@implementation TellTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
